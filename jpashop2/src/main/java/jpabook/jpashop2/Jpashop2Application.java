9c1891e368cc6d829cc32bf0ecc6a09cfb75e31c
권장순 <jsoonworld@gmail.com>
2023-05-08 00:56:18 +0900

    init_2nd

--- jpashop2/src/main/java/jpabook/jpashop2/Jpashop2Application.java
+++ jpashop2/src/main/java/jpabook/jpashop2/Jpashop2Application.java
@@ -7,7 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Jpashop2Application {
 
 	public static void main(String[] args) {
+
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+		System.out.println("data = " + data);
+
+
 		SpringApplication.run(Jpashop2Application.class, args);
+
+
 	}
 
 }
