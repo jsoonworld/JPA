4c8d7f1c257226aee0bdf364ec3e27a03e50e067
권장순 <jsoonworld@gmail.com>
2024-02-14 09:58:43 +0900

    feat: 고급 매핑 학습.

---
+++ 01. JPA-Basic/jpashop/src/main/java/jpabook/jpashop/domain/Album.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Album extends Item{
+
+    private String artist;
+    private String etc;
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getEtc() {
+        return etc;
+    }
+
+    public void setEtc(String etc) {
+        this.etc = etc;
+    }
+}
--- 01. JPA-Basic/jpashop/src/main/java/jpabook/jpashop/domain/Item.java
+++ 01. JPA-Basic/jpashop/src/main/java/jpabook/jpashop/domain/Item.java
@@ -7,7 +7,9 @@ import java.util.List;
 import java.util.Map;
 
 @Entity
-public class Item {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn
+public abstract class Item {
     @Id
     @GeneratedValue
     @Column(name = "ITEM_ID")
