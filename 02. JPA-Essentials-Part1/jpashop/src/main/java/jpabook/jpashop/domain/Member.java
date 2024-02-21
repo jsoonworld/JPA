e5ec54c7d5291291edd4111d9e86fca257dd8c9f
권장순 <jsoonworld@gmail.com>
2024-02-21 22:06:02 +0900

    feat: 도메인 분석 및 엔티티 클래스 개발
    - 요구사항 분석
    - 도메인 모델과 테이블 설계
    - 엔티티 클래스 개발
    - 엔티티 설계시 주의점

---
+++ 02. JPA-Essentials-Part1/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "memeber_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+
+
+}
---
+++ 02. JPA-Essentials-Part1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import static jakarta.persistence.FetchType.*;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice;
+    private int count;
+}
