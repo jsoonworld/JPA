0ffc3f4ef841e3f892d081be3974853a82cda4b2
권장순 <jsoonworld@gmail.com>
2024-03-11 11:08:20 +0900

    feat: init

---
+++ 03. JPA-Essentials-Part2/jpashop2/src/main/java/jpabook/jpashop/domain/item/Movie.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@DiscriminatorValue("M")
+@Getter
+@Setter
+public class Movie extends Item{
+
+    private String director;
+    private String actor;
+}
