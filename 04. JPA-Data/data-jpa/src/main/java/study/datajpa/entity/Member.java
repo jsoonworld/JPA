71d1b9f31aab54076f6c03850b3a355aaad4cf2b
권장순 <jsoonworld@gmail.com>
2024-03-25 17:37:10 +0900

    feat: 예제 도메인 모델 구현 및 테스트 케이스 작성

--- 04. JPA-Data/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ 04. JPA-Data/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,26 +1,42 @@
 package study.datajpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null){
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
