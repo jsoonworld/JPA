a0cae2432e8d4e7e13c0e7c11b336c17be48f6df
권장순 <jsoonworld@gmail.com>
2024-04-24 19:23:20 +0900

    feat: querydsl 기본 문법 학습

--- 05. Querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ 05. Querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,8 +1,10 @@
 package study.querydsl;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +23,14 @@ import static org.assertj.core.api.Assertions.*;
 @Transactional
 public class QuerydslBasicTest {
 
-    @Autowired
+    @PersistenceContext
     EntityManager em;
 
     JPAQueryFactory queryFactory;
 
     @BeforeEach
     public void before() {
+        queryFactory = new JPAQueryFactory(em);
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
         em.persist(teamA);
@@ -90,26 +93,40 @@ public class QuerydslBasicTest {
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
 
+
     @Test
-    public void resultFetch() {
-        List<Member> fetch = queryFactory
+    public void paging1() {
+        List<Member> result = queryFactory
                 .selectFrom(member)
+                .orderBy(member.username.desc()) .offset(1) //0부터 시작(zero index) .limit(2) //최대 2건 조회
                 .fetch();
-
-        Member fetchOne = queryFactory
-                .selectFrom(member)
-                .fetchOne();
-
-        Member fetchFirst = queryFactory
-                .selectFrom(member)
-                .fetchFirst();
-
-        QueryResults<Member> results = queryFactory
-                .selectFrom(member)
-                .fetchResults();
+        assertThat(result.size()).isEqualTo(2);
     }
 
-    
+    @Test
+    public void aggregation() throws Exception {
+        List<Tuple> result = queryFactory
+                .select(member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min())
+                .from(member)
+                .fetch();
+        Tuple tuple = result.get(0);
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.max())).isEqualTo(40);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+    }
+
+
+
+
+
+
+
 
 
 }
